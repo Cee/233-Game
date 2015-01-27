62631a29fac3d0ca6fe11b215b88691a57f86f0f
Cee <cee@chu2byo.com>
2015-01-27 18:43:16 +0800

    Drop logic

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ var icon_back;
 
 var rank_image;
 
+var game_bg;
+
+var game_refresh_timer = null;
+var game_logic_timer = null;
+var showingIcons = new Array();
+var duration = 3000.0;
+var icon_count = 0;
+var all_height = 550;
+var timePoint = 0;
+var speed = 800;
+
+status = 0;
+score = 0;
 
 function init() {
 
@@ -46,12 +59,14 @@ function loadImage() {
 	explain_image = new createjs.Bitmap("img/explain_bg.jpg");
 	icon_back = new createjs.Bitmap("img/icon_back.png");
 	rank_image = new createjs.Bitmap("img/rank_bg.jpg");
+	game_bg = new createjs.Bitmap("img/game_bg.jpg");
 
 	scale(index_image);
 	scale(btn_explain_normal);
 	scale(btn_rank_normal);
 	scale(btn_begin_normal);
 	scale(rank_image);
+	scale(game_bg);
 
 	btn_explain_normal.x = 30;
 	btn_explain_normal.y = 420;
@@ -114,7 +129,11 @@ function removeIndexPage() {
 /** Btn Click Handlers **/
 function handleStartClick() {
 	removeIndexPage();
-	// stage.update();
+	stage.addChild(game_bg);
+	$("#game").removeClass("hide");
+	stage.update();
+	// startGameLogic();
+	startGame();
 }
 
 function handleExplainClick() {
@@ -146,6 +165,7 @@ function handleExplainBackClick() {
 
 function handleIndexRankBackClick() {
 	$("#rank").addClass("hide");
+	$("#game").addClass("hide");
 	stage.removeChild(rank_image);
 	stage.removeChild(icon_back);
 	icon_back.removeAllEventListeners();
@@ -153,15 +173,120 @@ function handleIndexRankBackClick() {
 	stage.update();
 }
 
+/** Game **/
+function startGame() {
+    game_refresh_timer = setInterval(function() {
+    	// move
+    	$(".icon").each(function(index, element) {
+    		var node = showingIcons[index];
+    		var diff = timePoint - node["timePoint"];
+    		var percent = parseFloat(diff / node["duration"]);
+    		var top = all_height * percent;
+    		$(element).css("top", top);
+    	});
+
+    	// remove
+    	// var game_pool = $("#game");
+    	// $(".icon").each(function(index, element) {
+    	// 	if (parseInt($(element).css("top").replace('px', '')) > 480) {
+    	// 		console.log(element);
+    	// 		game_pool.removeChild(game_pool.childNodes[index]);
+    	// 		delete showingIcons[index];
+    	// 	}
+    	// });
+    	// $(showingIcons).each(function(index, element) {
+    	// 	if (element["top"] > 480) {
+    	// 		delete element;
+    	// 	}
+    	// })
+    	// add
+    	if (timePoint % speed == 0) {
+    		appendIcon();
+    	}
+    	timePoint += 10; // timePoint: ms
+    	// console.log(timePoint);
+    }, 10);
+}
+
+function startGameLogic(num) {
+	// game_logic_timer = setInterval(function() {
+		// console.log(click_numbers.length);
+		// if (click_numbers.length > 0) {
+			// var num = click_numbers.shift();
+			
+		// }
+	// }, 10);
+	console.log(status, num, score);
+	if (status == 0) {
+		if (parseInt(num) !== parseInt(2)) {
+			endGame();
+		}
+	} else if (status == 1) {
+		if (parseInt(num) !== parseInt(3)) {
+			endGame();
+		}
+	} else if (status == 2) {
+		if (parseInt(num) !== parseInt(3)) {
+			endGame();
+		} else {
+			score++;
+		}
+	}
+	status++;
+	status = status % 3;
+}
+
+function appendIcon() {
+	icon_count++;
+	var number = Math.floor(Math.random() * 2) + 2;
+	var icon_class = Math.floor(Math.random() * 4) + 1;
+	var left = Math.floor(Math.random() * 230);
+	var degree = Math.floor(Math.random() * 180) - 90;
+	var node = 	"<div class='icon_" + number + "_" + icon_class + " icon'" +
+				"style='margin-left:" + left + "px; " + rotate_icon(degree) + "; top: -150px;'" +
+				"id='" + number + "'" +
+				"></div>";
+	$("#game").append(node);
+	$("#game").children().last().click(function() {
+		$(this).addClass("hide");
+		startGameLogic(parseInt($(this).attr("id")));
+	});
+	// console.log($("#game").children().last());
+	// // $("#game:last-child").click(function() {
+	// // 	click_numbers.push(parseInt($(this).attr("id")));
+	// // 	$(this).addClass("hide");
+	// // });
+	var showingIcon = {
+		"index": icon_count,
+		"top": -150,
+		"timePoint": timePoint,
+		"duration": duration
+	};
+	showingIcons.push(showingIcon);
+}
+
+function endGame() {
+	alert("End");
+}
 /** Scale **/
 function scale(obj) {
 	obj.scaleX = .5;
 	obj.scaleY = .5;
 }
 
+/** Rotate **/
+function rotate_icon(degree) {
+	var str = 
+			"-ms-transform: rotate(" + degree + "deg); /* IE 9 */" +
+    		"-webkit-transform: rotate(" + degree + "deg); /* Chrome, Safari, Opera */" +
+    		"transform: rotate(" + degree + "deg);";
+    return str;
+}
 
+/** UI **/
 function adjust() {
 	$("#rank").css("margin-left", $(window).width() / 2 - 140);
+	$("#game").css("margin-left", $(window).width() / 2 - 130);
 }
 
 $(document).ready(function() {
@@ -171,4 +296,8 @@ $(document).ready(function() {
     document.ontouchmove = function(event){
     	event.preventDefault();
 	}
+	// $(".icon").click(function() {
+	// 	click_numbers.push(parseInt($(this).attr("id")));
+	// 	$(this).addClass("hide");
+	// });
 });
